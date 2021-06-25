1a6406c393657a656087ed4c7c67699deddbcb54
Anvi Agarwal <85110078+anviAgarwal@users.noreply.github.com>
2021-06-25 12:00:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var grid = 50;
 var width = 1366;
 var carGroup1,logGroup1;
 var grassHeight = 100;
-
+//var player;
 var carAnimation, logAnimation, playerAnimation;
 var school;
 function preload()
@@ -11,13 +11,13 @@ function preload()
 }
 
 function setup() {
-  createCanvas(1366,2700);
+  createCanvas(1366,700);
   carGroup1 = new Group();
   logGroup1 = new Group();
   
   for (var i=0;i<6;i++) {
     var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
-  
+    player=new player(widht/2,height-25);
   if (i%2===0){
   var road=createSprite(683,height-150-(i*400)-grassHeight,width,300)
   road.shapeColor="black";
@@ -25,7 +25,7 @@ function setup() {
 bottomGrass1.shapeColor="grey";
  }
  for (var i=0;i<40;i++) {
-  cars=new Car (2);
+  car=new Car (2);
   carGroup1.add(car.spt);
 }
 for (var i =0;i<40;i++) {
@@ -36,33 +36,26 @@ for (var i =0;i<40;i++) {
 }
 function draw() {
   background("skyblue");
+  translate (0,-player.spt.y+height-150);
   for(i=1;i<logGroup1.length;i++){
     if (logGroup1[i].x<0)
-    
-        logGroup1[i].x=width;
+    logGroup1[i].x=width;
     }
 
 for(i=1;i<carGroup1.length;i++){
   if(carGroup1[i].x<0)
   {
-  if (carGroup1[i].x<0)
-  {
-      carGroup1[i].x=width;
+  carGroup[i].x=width;
   }
+  
 
-if (carGroup1[i].x>width)
-{
+if (carGroup1[i].x>width){
+
+
     carGroup1[i].x=0;
 }
 }   
-if(carGroup1.isTouching(player.spt)){
-  player.spt.x = width/2;
-  player.spt.y = height-75;
-}
-if(logGroup1.isTouching(player.spt)){
-  player.spt.x = player.spt.x-3;
-}
-} 
+
  
   
  
@@ -70,3 +63,14 @@ if(logGroup1.isTouching(player.spt)){
   drawSprites();
 
 }
+//function keyPressed(){
+ // if(keyCode == UP_ARROW){
+ //   player.move(0,-2);
+ // }else if(keyCode == DOWN_ARROW){
+ //   player.move(0,2);
+ // }else if(keyCode == LEFT_ARROW){
+//    player.move(-2,0);
+ // }else if(keyCode == RIGHT_ARROW){
+   // player.move(2,0);
+ //// }
+//}
